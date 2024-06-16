b3235d72083ebf32e45e7d77a9be7db95c0b3c18
Srinjoy Roy <srinjoy12345@gmail.com>
2024-06-16 14:06:29 +0530

    Initial version of SignUp page added
    frontend only

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './pages/Login'
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 function App() {
 
 
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path='/' Component={Login}></Route>
+          <Route path='/signup' Component={SignUp}></Route>
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
